0c2e9d034013ef6f859f1b3be8f718d72765374e
WellingtonFidelis <wellingtonf20@gmail.com>
2021-05-14 22:05:52 -0300

    Development dynamic rate

--- frontend/movie-rater/src/components/movie-details.js
+++ frontend/movie-rater/src/components/movie-details.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 export default function MovieDetails(props) {
+  const { movie } = props;
+  const [highlighted, setHighlighted] = useState(-1);
+
+  const highlightRate = high => event => {
+    setHighlighted(high);
+  }
+
+  const rateClicked = rate => event => {
+    fetch(`http://127.0.0.1:8000/api/movies/${movie.id}/rate_movie/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        //'Authorization': 'Token 97621ce2baeb11f722db66ad0ecf1ce78898361b',
+        'Authorization': 'Token 8f902b9af361c445af29f69aa1683ac4fb44061a'
+      },
+      body: JSON.stringify({
+        stars: rate + 1,
+      }),
+    })
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
+  }
 
   return (
-    <div>
-      {props.movie ? (
+    <React.Fragment>
+      {movie ? (
         <div>
-          <h1>{props.movie.title}</h1>
-          <p>{props.movie.description}</p>
-          <FontAwesomeIcon icon={faStar} />
+          <h1>{movie.title}</h1>
+          <p>{movie.description}</p>
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 0 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 1 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 2 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 3 ? 'orange' : ''} />
+          <FontAwesomeIcon icon={faStar} className={movie.avg_rating > 4 ? 'orange' : ''} />
+          ({movie.no_of_ratings})
+          <div className="rate-container">
+            <h2>Rate it</h2>
+            {
+              [...Array(5)].map((element, index) => {
+                return <FontAwesomeIcon
+                  key={index}
+                  icon={faStar}
+                  className={highlighted > index - 1 ? 'purple' : ''}
+                  onMouseEnter={highlightRate(index)}
+                  onMouseLeave={highlightRate(-1)}
+                  onClick={rateClicked(index)}
+                />
+              })
+            }
+          </div>
         </div>
       ) : null}
-      
-    </div>
+    </React.Fragment>
   );
 }
